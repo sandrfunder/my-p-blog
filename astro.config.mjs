17b5f9287c8bf9fb71c2da09ce2f3c2a3e75f877
Odhy Pradhana <hello@odhyp.com>
2026-06-04 10:40:16 +0700

    chore: add deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+// integrations
+import tailwindcss from "@tailwindcss/vite"
+import mdx from "@astrojs/mdx"
+import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
+
+// Expressive Code
+import expressiveCode from "astro-expressive-code"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    expressiveCode({
+      themes: ["github-dark"],
+    }),
+    mdx(),
+    icon(),
+    sitemap(),
+  ],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
